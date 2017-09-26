9e872b7407b1f08f1581bc44bc9235d1002472ee
Camden Phalen <camdenphalen@gmail.com>
2017-09-25 22:09:15 -0400

    package installation and structure setup

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Camden Website</h2>
+        </div>
+        <p>
+          It's coming right along.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
